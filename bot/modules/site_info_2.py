6fbec22c52107f6abd3d03c7f99e5f5fd99038ab
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-02-25 17:27:20 +0600

    Update site_info_2.py

--- bot/modules/site_info_2.py
+++ bot/modules/site_info_2.py
@@ -14,18 +14,6 @@ async def save_note(client, message):
     if not DATABASE_URL:
         return await message.reply("No database added.")
 
-    client = MongoClient(DATABASE_URL)
-    db = client.grameenphone
-    collection = db.grameenphone_site_info
-    note_name = message.text.split(maxsplit=1)
-    if len(note_name) < 2:
-        await message.reply("Please provide a note name.")
-        return
-    site_code = note_name[1].strip()
-
-    if message.reply_to_message:
-        site_info = message.reply_to_message.text
-        collection.insert_one({"site_code": site_code, "site_info": site_info})
-        await message.reply(f"{site_code} info saved in database")
-    else:
-        await message.reply("Please reply to a message to save it as a note.")
+    db = client["GP_FULL"]
+    collection = db["gp_site_info"]
+    
