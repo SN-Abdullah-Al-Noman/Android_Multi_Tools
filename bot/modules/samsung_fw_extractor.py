a1c34ed2d1b6a7aa33c87ead22efdd3b3212d24c
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-10-13 19:38:14 +0600

    Update samsung_fw_extractor.py

--- bot/modules/samsung_fw_extractor.py
+++ bot/modules/samsung_fw_extractor.py
@@ -64,35 +64,26 @@ async def samsung_fw_extract(client, message):
         await editMessage(status, banner)
         return
 
-    for file in os.listdir(DOWNLOAD_DIR):
-        if file.endswith(".tar.md5"):
-            old_path = os.path.join(DOWNLOAD_DIR, file)
-            new_name = os.path.join(DOWNLOAD_DIR, file.replace(".md5", ""))
-            os.rename(old_path, new_name)
-
     banner = f"\n{banner}\n<b>Step 2:</b> Extracting tar files."
     await editMessage(status, banner)
-    for file in os.listdir(DOWNLOAD_DIR):
-        try:
-            if file.endswith(".tar"):
-                subprocess.run(["7z", "x", f"{DOWNLOAD_DIR}/{file}", f"-o{DOWNLOAD_DIR}", "-aoa"])
-        except Exception as e:
-            banner = f"\n{banner}\n{e}."
-            await editMessage(status, banner)
-            return
-
+    try:
+        subprocess.run('for file in *.tar; do tar -xvf "$file"; done', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('find . -type f ! -name 'super.img.lz4' ! -name 'optics.img.lz4' ! -name 'prism.img.lz4' ! -name 'boot.img.lz4' -delete', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf *.tar', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf meta-data', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
 
     banner = f"\n{banner}\n<b>Step 3:</b> Extracting lz4 files."
     await editMessage(status, banner)
-    lz4_files = [file for file in os.listdir(DOWNLOAD_DIR) if file.endswith(".lz4")]
-    for file in lz4_files:
-        lz4_file_path = os.path.join(DOWNLOAD_DIR, file)
-        try:
-            subprocess.run(["lz4", "-d", lz4_file_path, f"-o{DOWNLOAD_DIR}"])
-        except Exception as e:
-            banner = f"\n{banner}\n{e}."
-            await editMessage(status, banner)
-            return
+    try:
+        subprocess.run('for file in *.lz4; do lz4 -d "$file" "${file%.lz4}"; done', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
 
     banner = f"\n{banner}\n<b>Step 4:</b> Converting sparse super.img to raw super.img."
     await editMessage(status, banner)
@@ -108,19 +99,19 @@ async def samsung_fw_extract(client, message):
     banner = f"\n{banner}\n<b>Step 5:</b> Extracting all partitions from super.img"
     await editMessage(status, banner)
     try:
-        subprocess.run("lpunpack super.img", shell=True, cwd=DOWNLOAD_DIR)
-        subprocess.run("rm -rf super.img", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('lpunpack super.img', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf super.img', shell=True, cwd=DOWNLOAD_DIR)
     except Exception as e:
-            banner = f"\n{banner}\n{e}."
-            await editMessage(status, banner)
-            return
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
 
     banner = f"\n{banner}\n<b>Step 6:</b> Compressing all img to xz level 9."
     await editMessage(status, banner)
     try:
         subprocess.run('for i in *.img; do 7z a -mx9 "${i%.*}.img.xz" "$i"; done && rm -rf *.img', shell=True, cwd=DOWNLOAD_DIR)
     except Exception as e:
-        subprocess.run("rm -rf *.img", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf *.img', shell=True, cwd=DOWNLOAD_DIR)
         banner = f"\n{banner}\n{e}."
         await editMessage(status, banner)
         return
@@ -128,7 +119,7 @@ async def samsung_fw_extract(client, message):
     banner = f"\n{banner}\n<b>Step 7:</b> Compressing all files into single zip."
     await editMessage(status, banner)
     try:
-        subprocess.run("7z a -tzip -mx=9 archive.zip *", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('7z a -tzip -mx=9 archive.zip *', shell=True, cwd=DOWNLOAD_DIR)
     except Exception as e:
         banner = f"\n{banner}\n{e}."
         await editMessage(status, banner)
@@ -138,6 +129,6 @@ async def samsung_fw_extract(client, message):
     await editMessage(status, banner)
     banner = f"\n{banner}\n\n<b>File Uploading Completed.</b>\nHere is file link."
     await editMessage(status, banner)
-    subprocess.run("rm -rf *", shell=True, cwd=DOWNLOAD_DIR)
+    subprocess.run("rm -rf *.zip", shell=True, cwd=DOWNLOAD_DIR)
 
 bot.add_handler(MessageHandler(samsung_fw_extract, filters=command("fw")))
