588b040b504dd23567e1dc856ba6fbd3fee17d39
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-10-13 18:31:15 +0600

    Update samsung_fw_extractor.py

--- bot/modules/samsung_fw_extractor.py
+++ bot/modules/samsung_fw_extractor.py
@@ -46,7 +46,7 @@ async def samsung_fw_extract(client, message):
     await editMessage(status, banner)
 
     try:
-        subprocess.run(['wget', '-O', 'fw.zip', '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"', 'link'], cwd=DOWNLOAD_DIR)
+        subprocess.run(['wget', '-O', 'fw.zip', '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"', f'{link}'], cwd=DOWNLOAD_DIR)
     except Exception as e:
         banner = f"\n{banner}\nFailed: {e}."
         await editMessage(status, banner)
