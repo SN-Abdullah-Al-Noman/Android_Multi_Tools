80df46d6849a7f136ebb901625860d5d48ddd4b4
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-10-13 18:21:09 +0600

    Update samsung_fw_extractor.py

--- bot/modules/samsung_fw_extractor.py
+++ bot/modules/samsung_fw_extractor.py
@@ -57,7 +57,7 @@ async def samsung_fw_extract(client, message):
     banner = f"\n{banner}\n<b>Step 1:</b> Extracting firmware zip."
     await editMessage(status, banner)
     try:
-        "subprocess.run(['7z', 'x', 'fw.zip'], cwd=DOWNLOAD_DIR)
+        subprocess.run(['7z', 'x', 'fw.zip'], cwd=DOWNLOAD_DIR)
     except Exception as e:
         banner = f"\n{banner}\nFailed: {e}."
         await editMessage(status, banner)
