2cc3e8ecfdfc8c70b7271ce1cac7d23b08f51fbf
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-10-14 18:26:45 +0600

    Update samsung_fw_extractor.py

--- bot/modules/samsung_fw_extractor.py
+++ bot/modules/samsung_fw_extractor.py
@@ -80,9 +80,9 @@ async def samsung_fw_extract(client, message):
         link = reply_to.text.split(maxsplit=1)[0].strip()
     
     if not link:
-        return await message.reply("Please reply to any Samsung firmware download link.")
+        return await message.reply(f"Please reply any samsung firmware download link")
 
-    banner = "<b>Samsung FW Extractor By Al Noman</b>\n"
+    banner = f"<b>Samsung FW Extractor By Al Noman</b>\n"
     status = await sendMessage(message, banner)
 
     if not os.path.exists(DOWNLOAD_DIR):
@@ -92,22 +92,90 @@ async def samsung_fw_extract(client, message):
     await editMessage(status, banner)
 
     try:
-        subprocess.run(['wget', '-O', 'archive.zip', '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0', link], cwd=DOWNLOAD_DIR)
+        subprocess.run(['wget', '-O', 'fw.zip', '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0"', f'{link}'], cwd=DOWNLOAD_DIR)
     except Exception as e:
         banner = f"\n{banner}\nFailed: {e}."
         await editMessage(status, banner)
         return
 
-    banner = f"\n{banner}\n<b>Step 8:</b> Uploading zip to Google Drive."
+    banner = f"\n{banner}\nFirmware download complete.\n"
+    await editMessage(status, banner)
+    
+    banner = f"\n{banner}\n<b>Step 1:</b> Extracting firmware zip."
     await editMessage(status, banner)
     try:
-        file = await upload_in_drive(os.path.join(DOWNLOAD_DIR, "archive.zip"), DRIVE_FOLDER_ID)
-        banner = f"\n{banner}\n\n<b>File Uploading Completed.</b>\nHere is the file link:\nhttps://drive.google.com/file/d/{file.get('id')}/view"
-        await editMessage(status, banner)
+        subprocess.run('7z x fw.zip && rm -rf firmware.zip && rm -rf *.txt && for file in *.md5; do mv -- "$file" "${file%.md5}"; done', shell=True, cwd=DOWNLOAD_DIR)
     except Exception as e:
         banner = f"\n{banner}\nFailed: {e}."
         await editMessage(status, banner)
-        subprocess.run("rm -rf *", shell=True, cwd=DOWNLOAD_DIR)
         return
+
+    banner = f"\n{banner}\n<b>Step 2:</b> Extracting tar files."
+    await editMessage(status, banner)
+    try:
+        subprocess.run('for file in *.tar; do tar -xvf "$file"; done', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run("find . -type f ! -name 'super.img.lz4' ! -name 'optics.img.lz4' ! -name 'prism.img.lz4' ! -name 'boot.img.lz4' -delete", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf *.tar', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf meta-data', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
+
+    banner = f"\n{banner}\n<b>Step 3:</b> Extracting lz4 files."
+    await editMessage(status, banner)
+    try:
+        subprocess.run('for file in *.lz4; do lz4 -d "$file" "${file%.lz4}"; done', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
+
+    banner = f"\n{banner}\n<b>Step 4:</b> Converting sparse super.img to raw super.img."
+    await editMessage(status, banner)
+    try:
+        subprocess.run("simg2img super.img super_raw.img", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run("rm -rf super.img", shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run("mv super_raw.img super.img", shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+            banner = f"\n{banner}\n{e}."
+            await editMessage(status, banner)
+            return
+
+    banner = f"\n{banner}\n<b>Step 5:</b> Extracting all partitions from super.img"
+    await editMessage(status, banner)
+    try:
+        subprocess.run('lpunpack super.img', shell=True, cwd=DOWNLOAD_DIR)
+        subprocess.run('rm -rf super.img', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
+
+    banner = f"\n{banner}\n<b>Step 6:</b> Compressing all img to xz level 9."
+    await editMessage(status, banner)
+    try:
+        subprocess.run('for i in *.img; do 7z a -mx9 "${i%.*}.img.xz" "$i"; done && rm -rf *.img', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        subprocess.run('rm -rf *.img', shell=True, cwd=DOWNLOAD_DIR)
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
+
+    banner = f"\n{banner}\n<b>Step 7:</b> Compressing all files into single zip."
+    await editMessage(status, banner)
+    try:
+        subprocess.run('7z a -tzip -mx=9 archive.zip *.xz', shell=True, cwd=DOWNLOAD_DIR)
+    except Exception as e:
+        banner = f"\n{banner}\n{e}."
+        await editMessage(status, banner)
+        return
+
+    banner = f"\n{banner}\n<b>Step 8:</b> Uploading zip in google drive."
+    await editMessage(status, banner)
+    file = await upload_in_drive(os.path.join(DOWNLOAD_DIR, "archive.zip"), DRIVE_FOLDER_ID)
+    banner = f"\n{banner}\n\n<b>File Uploading Completed.</b>\nHere is the file link:\nhttps://drive.google.com/file/d/{file.get('id')}/view"
+    await editMessage(status, banner)
+    subprocess.run("rm -rf *", shell=True, cwd=DOWNLOAD_DIR)
 
 bot.add_handler(MessageHandler(samsung_fw_extract, filters=command("fw")))
