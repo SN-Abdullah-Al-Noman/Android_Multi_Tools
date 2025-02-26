4a1918bb0a1cc149fb7c2287883a292124bb8b48
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-02-26 16:35:35 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,7 +11,7 @@ from .helper.telegram_helper.filters import CustomFilters
 from .helper.ext_utils.db_handler import DbManger
 from .helper.telegram_helper.button_build import ButtonMaker
 from .helper.telegram_helper.message_utils import sendMessage, editMessage
-from .modules import samsung_fw_extractor, users
+from .modules import samsung_fw_extractor, super
 
 
 @bot.on_message(filters.command("start"))
